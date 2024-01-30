e16e2d33829475fcb644edc82f4dffd7515cdd86
dipak0499 <158210628+dipak0499@users.noreply.github.com>
2024-01-31 00:07:03 +0530

    Base calculator  create using python

---
+++ base calculator create.py
@@ -0,0 +1,22 @@
+#Create a console base calculator.
+
+'''num1=eval(input("enter your first number:-"))
+num2=eval(input("enter your second number:-"))
+operation=input(''' '+' for addition, '-' for sub, '*' for multiplication, '/' for division''')
+if operation=='+':
+    output= num1+num2
+    print(output)
+if operation=='-':
+    output=num1-num2
+    print(output)
+if operation=='*':
+    output= num1*num2
+    print(output)
+if operation=='/':
+    output=num1/num2
+    print(output)'''
+
+    
+
+
+
